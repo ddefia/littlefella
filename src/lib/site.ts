89572eecf64878b00114ab7314dd56b8f7ccff30
Anthony Bassi <185571380+ddefia@users.noreply.github.com>
2026-08-16 23:09:23 -0400

    Show info@littlefella.vc as the public address.

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,7 +2,7 @@ export const SITE = {
   name: "little fella",
   legal: "little fella ventures",
   short: "lfv",
-  email: "anthony@littlefella.vc",
+  email: "info@littlefella.vc",
 } as const;
 
 export const SECTORS = [
